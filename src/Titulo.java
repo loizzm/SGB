772dc82227480b9697b719dff033b7f7574ceace
loizzm <ireliontt@gmail.com>
2023-06-03 13:23:33 -0300

    Implementação inicial

--- src/Titulo.java
+++ src/Titulo.java
@@ -8,7 +8,48 @@ class Titulo{
     int Tempo_emprestado;
 
     String get_Descricao(){
-        return 
+        return this.Descricao;
     }
+    String get_Nome(){
+        return this.Nome;
+    }
+     void set_Descricao(String Nome){
+        this.Descricao=Nome;
+     }
+     void set_Nome(String Nome){
+        this.Nome=Nome;
+     }
+     int get_Tempo_Emprestimo(){
+        return this.Tempo_de_emprestimo;
+    }
+    void set_Tempo_Emprestimo(int Tempo){
+        this.Tempo_de_emprestimo=Tempo;
+    }
+    int get_ID_livro(){
+        return this.ID_livro;
+    }
+    void set_ID_livro(int id){
+        this.ID_livro=id;
+    }
+    int get_ID_Usuario(){
+        return this.ID_usuario;
+    }
+    void set_ID_Usuario(int id_usuario){
+        this.ID_usuario=id_usuario;
+    }
+    int get_Tipo_titulo(){
+        return this.tipo_titulo;
+    }
+    void set_Tipo_titulo(int tipo){
+        this.tipo_titulo=tipo;
+    }
+    int get_Tempo_emprestado(){
+        return this.Tempo_emprestado;
+    }
+    void set_Tempo_emprestado(int tempo){
+        this.Tempo_emprestado=tempo;
+    }
+    
+    
 
 }

ec6b86cfbcf667972233a2215f98669d9e9c0cd9
loizzm <ireliontt@gmail.com>
2023-06-03 12:45:13 -0300

    Commit inicial

---
+++ src/Titulo.java
@@ -0,0 +1,14 @@
+class Titulo{
+    String Descricao;
+    String Nome;
+    int Tempo_de_emprestimo;
+    int ID_livro;
+    int ID_usuario;
+    int tipo_titulo;
+    int Tempo_emprestado;
+
+    String get_Descricao(){
+        return 
+    }
+
+}
